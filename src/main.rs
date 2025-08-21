48e9bb71d7b6f978c4a89a9e8d1e44fb4e286695
Nolekev1214 <nolekev1214@gmail.com>
2025-08-21 16:49:25 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+#![no_std]
+#![no_main]
+
+use embassy_executor::Spawner;
+use embassy_time::{Duration, Timer};
+use {defmt_rtt as _, panic_probe as _};
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) -> ! {
+    let p = embassy_rp::init(Default::default());
+    let mut led = Output::new(p.PIN01, Level::Low);
+
+    loop {
+        info!("led on!");
+        led.set_high();
+        Timer::after_secs(1).await;
+
+        info!("led off!");
+        led.set_low();
+        Timer::after_secs(1).await;
+    }
+}
