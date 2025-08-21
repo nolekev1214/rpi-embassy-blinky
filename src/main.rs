c0e3f5fba22236f6bd0aa800dba394b2a696dd69
Nolekev1214 <nolekev1214@gmail.com>
2025-08-21 18:49:51 -0400

    actually compiles now

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,21 @@
 #![no_main]
 
 use embassy_executor::Spawner;
-use embassy_time::{Duration, Timer};
+use embassy_rp::gpio::{Level, Output};
+use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) -> ! {
     let p = embassy_rp::init(Default::default());
-    let mut led = Output::new(p.PIN01, Level::Low);
+    let mut led = Output::new(p.PIN_0, Level::Low);
 
     loop {
-        info!("led on!");
+        defmt::info!("led on!");
         led.set_high();
         Timer::after_secs(1).await;
 
-        info!("led off!");
+        defmt::info!("led off!");
         led.set_low();
         Timer::after_secs(1).await;
     }
